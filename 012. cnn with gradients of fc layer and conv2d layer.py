ad476597b7aa217166f11906c780f7012fca6355
ihateandreykrasnokutsky <gef711@gmail.com>
2025-07-10 00:15:40 +0300

    Forward propagation functions. Added the definitions of the forward pass functions. Tried to undestand everything and write everything by heart and with understanding.

--- 012. cnn with gradients of fc layer and conv2d layer.py
+++ 012. cnn with gradients of fc layer and conv2d layer.py
@@ -1,2 +1,42 @@
-#I'll rewrite all the code manually and will try to understand everyting again. The version of the code is slightly different from the previous one in its appearance.
 import numpy as np
+#forward pass functions
+def conv2d(image,kernel):
+    h,w=image.shape
+    kh,kw=kernel.shape
+    out_h=h-kh+1
+    out_w=w-kw+1
+    output=np.zeros((out_h,out_w))
+    for i in range(out_h):
+        for j in range(out_w):
+            region=image[i:i+kh, j:j+kw]
+            output[i,j]=np.sum(region*kernel)
+    return output
+
+def relu(x):
+    return np.max(0,x)
+
+def max_pooling(x, size=2, stride=2):
+    h,w=x.shape
+    out_h=h//size
+    out_w=w//size
+    output=np.zeros((out_h,out_w))
+    for i in range(out_h):
+        for j in range (out_w):
+            region=x[i*size:i*size+stride, j*size:j*size+stride]
+            output[i,j]=np.max(region)
+    return output
+
+def faltten(x):
+    return x.flatten()
+
+def fully_connected(x, weight, bias):
+    return np.dot(weight,x)+bias
+
+def softmax(x):
+    exps=np.exp(x-np.max(x)) #preventing numerical instability
+    return exps/np,sum(exps)
+
+def cross_entropy_loss(probs, label):
+    return -np.log(probs[label]+1e-10)
+
+#-----------------------OK, next time writing the backward pass functions!------------------------------------------------------
