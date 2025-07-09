a3019fc733eaa8091a9eb76d7deb52a0f70477bb
ihateandreykrasnokutsky <gef711@gmail.com>
2025-07-09 20:33:26 +0300

    creating the 2nd cnn studying file This file contains the code given to me by the qwen LLM, and it implements the backpropagation of the fully connected layer and the convolutional layer (these are the main subjects I learn now).

---
+++ 012. cnn with gradients of fc layer and conv2d layer.py
@@ -0,0 +1,2 @@
+#I'll rewrite all the code manually and will try to understand everyting again. The version of the code is slightly different from the previous one in its appearance.
+import numpy as np
